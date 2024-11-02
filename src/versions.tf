750432b26e6e2431f300c575222b813be9520ce0
Igor Rodionov <goruha@gmail.com>
2024-11-02 19:17:28 +0100

    Initial commit

--- src/versions.tf
+++ src/versions.tf
@@ -1,5 +1,18 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 0.13.0"
 
-  required_providers {}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.9.0"
+    }
+    datadog = {
+      source  = "datadog/datadog"
+      version = ">= 3.19"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = ">= 2.1.0"
+    }
+  }
 }
